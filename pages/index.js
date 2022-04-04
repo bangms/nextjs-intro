863d7717d5ebbb0affedb5399cd53dc07f6f4aae
DESKTOP-EJPIOLD\dev_byj <myosikb@gmail.com>
2022-04-04 16:46:08 +0900

    api 연결해서 제목 불러오기

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,32 @@
-// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // import NavBar from "../components/NabBar";
 // import Head from "next/head";
 import Seo from "../components/Seo";
 
+const API_KEY = "10923b261ba94d897ac6b81148314a3f";
+
 export default function Home () {
-    // const [counter, setCounter] = useState(0);
-    // return "hi";
+    const [movies, setMovies] = useState();
+    useEffect(() => {
+        (async () => {
+        const { results } = await (
+            await fetch(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+            )
+        ).json();
+        setMovies(results);
+        })();
+    }, []);
+
    return(
         <div>
             <Seo title="Home" />
-            <h1 className="  ">Hello</h1>
-            {/* <NavBar />
-            <h1>Hello</h1> */}
-            {/* <h1> Hello {counter} </h1> */}
-            {/* <button onClick={() => setCounter((prev) => prev + 1)}>+</button> */}
-            {/* <style jsx global>{`
-                a {
-                    color: white;
-                }
-            `}</style> */}
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map((movie) => (
+                <div key={movie.id}>
+                    <h4>{movie.original_title}</h4>
+                </div>
+            ))}
         </div>
    )     
 }
